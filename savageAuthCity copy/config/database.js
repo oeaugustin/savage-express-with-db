47b413167ab10c38904d98ed05e6fdc66c19af86
Omar Augustin <35378562+oeaugustin@users.noreply.github.com>
2018-07-07 21:59:22 -0400

    Add files via upload

---
+++ savageAuthCity copy/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://demo:demodemo1@ds127781.mlab.com:27781/savagery' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
